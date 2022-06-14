192edfd04c7b4c286a98cc865914a6e6e36d1f5c
Priyam Dua <duap@uwindsor.ca>
2022-06-14 14:36:10 -0400

    implements runnable using lambda

---
+++ src/com/learn/java8/Main.java
@@ -0,0 +1,15 @@
+package com.learn.java8;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+		
+	
+		
+	}
+
+}
---
+++ src/com/learn/java8/lambdas/RunnableLambdaExample.java
@@ -0,0 +1,29 @@
+package com.learn.java8.lambdas;
+
+public class RunnableLambdaExample {
+
+	public static void main(String[] args) {
+
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Inside Runnable 1");
+			}
+		};
+
+		Thread t = new Thread(runnable);
+
+		t.start();
+
+		// now using java8
+
+		Runnable r = () -> System.out.println("Inside Runnable 2");
+
+		new Thread(r).start();
+		
+		new Thread(() ->System.out.println("Inside Runnabel 3")).start();
+
+	}
+
+}
