806ac730db63fb8242efb92b85b61196974cec21
Priyam Dua <duap@uwindsor.ca>
2022-06-30 15:28:48 -0400

    example of never to use mutable variable with parallel stream

---
+++ src/com/learn/java8/parallelstream/Sum.java
@@ -0,0 +1,21 @@
+package com.learn.java8.parallelstream;
+
+public class Sum {
+
+	private int total;
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public void performSum(int input) {
+
+		total += input;
+
+	}
+
+}
